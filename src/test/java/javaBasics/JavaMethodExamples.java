f7c3f18c63f1fdcb2fec273f2ab984ac3d611180
Kesavan <dhejokes@amazon.com>
2025-11-26 23:05:40 +0530

    java method examples

--- src/test/java/javaBasics/JavaMethodExamples.java
+++ src/test/java/javaBasics/JavaMethodExamples.java
@@ -2,8 +2,26 @@ package javaBasics;
 
 public class JavaMethodExamples {
 
+	public static void name(String inputString) {
+		
+		System.out.println("Hi,"+inputString+"!");
+		
+	}
+	
+	public void notAStaticMethod() {
+		System.out.println("I am not a static method");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		name("John");
+		JavaMethodExamples.name("Is it possible");
+		
+		JavaMethodExamples obj = new JavaMethodExamples();
+		
+		obj.notAStaticMethod();
+		
+		
 
 	}
 
